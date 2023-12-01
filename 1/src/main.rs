05bbf56e478aebb9dbef37419e2b0d1c516c5407
Harry Goatman <hg@harrygoatman.com>
2023-12-01 08:57:47 +0000

    style(1): refactor part 1

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -12,35 +12,31 @@ fn main() {
     env_logger::init();
 
     let input = load_input();
+    let lines: Vec<String> = input.split('\n').map(|line| line.to_string()).collect();
 
-    let filtered_lines: Vec<Vec<char>> = input
-        .split('\n')
-        .map(|line| line.chars().filter(|char| char.is_numeric()).collect())
+    let first_values = lines
+        .iter()
+        .map(|line| line.chars().find(|char| char.is_numeric()));
+    let last_values = lines
+        .iter()
+        .map(|line| line.chars().rev().find(|char| char.is_numeric()));
+
+    let calibration_value_pairs: Vec<String> = first_values
+        .zip(last_values)
+        .map(
+            |(maybe_first, maybe_last)| match (maybe_first, maybe_last) {
+                (Some(first), Some(last)) => Some([first, last].iter().collect::<String>()),
+                (_, _) => None,
+            },
+        )
+        .flat_map(|e| e)
         .collect();
 
-    log::debug!("Filtered lines: {:?}", filtered_lines);
+    log::debug!("Value Pairs: {:?}", calibration_value_pairs);
 
-    let calibration_values: Vec<u16> = filtered_lines
+    let calibration_values: Vec<u16> = calibration_value_pairs
         .iter()
-        .map(|unparsed| {
-            let mut iter = unparsed.iter();
-
-            let first_value = iter.nth(0);
-
-            if first_value.is_none() {
-                return None;
-            }
-
-            let first_value = first_value.unwrap();
-
-            let value: String = [first_value, iter.last().unwrap_or(first_value)]
-                .into_iter()
-                .collect();
-
-            Some(value)
-        })
-        .filter_map(|e| e)
-        .filter_map(|unparsed| unparsed.parse::<u16>().ok())
+        .filter_map(|unparsed| unparsed.parse().ok())
         .collect();
 
     log::debug!("Calibrations values: {:?}", calibration_values);
